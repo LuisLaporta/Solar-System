4291ceef8efb6581eed8633eb7fb20789d9d0af0
LuisLaporta <lui.laporta99@gmail.com>
2023-08-17 14:20:56 -0300

    feat: estlização do componente planetCard

--- src/components/PlanetCard.jsx
+++ src/components/PlanetCard.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../css/planetCard.css';
 
 class PlanetCard extends React.Component {
   render() {
     const { planetName } = this.props;
     const { planetImage } = this.props;
     return (
-      <div data-testid="planet-card">
-        <p data-testid="planet-name">{ planetName }</p>
-        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+      <div data-testid="planet-card" className="planet-form">
+        <div className="form-imgPlanet">
+          <img
+            src={ planetImage }
+            alt={ `Planeta ${planetName}` }
+            className={ planetName }
+          />
+        </div>
+        <div className="form-namePlanet">
+          <p data-testid="planet-name">{ planetName }</p>
+        </div>
       </div>
     );
   }
